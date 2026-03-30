3fdd015a15fefdff06910328deeff95e681ffb6c
hugo-belaloui <hugo.belaloui@laplateforme.io>
2026-03-30 14:00:51 +0200

    Job 0 done

---
+++ jour01/job00/Main.java
@@ -0,0 +1,22 @@
+package jour01.job00;
+public class Main
+
+{
+    public static void main(String[] args)
+    {
+        int TOTO = 18;
+        char c = 't';
+        String sentence = "Hello World";
+        long lot = 314159589;
+        float virg = 3.12635f;
+        boolean bool = true;
+
+        System.out.println(TOTO);
+        System.out.println(c);
+        System.out.println(sentence);
+        System.out.println(lot);
+        System.out.println(virg);
+        System.out.println(bool);
+
+    }
+}
